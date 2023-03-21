b129f1ccd8116933f2052c9fb018cf745f805031
Jonathan Heriyanto <jonathan.heriyanto@gmail.com>
2023-03-21 15:09:46 +0700

    Initial Commit

---
+++ memogit/memogitApp.swift
@@ -0,0 +1,17 @@
+//
+//  memogitApp.swift
+//  memogit
+//
+//  Created by Jonathan Heriyanto on 21/03/23.
+//
+
+import SwiftUI
+
+@main
+struct memogitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
