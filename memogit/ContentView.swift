8fdeac0bf72ff0b320864d727d4f3db697a29ed5
Jonathan Heriyanto <jonathan.heriyanto@gmail.com>
2023-03-21 15:16:27 +0700

    commit

--- memogit/ContentView.swift
+++ memogit/ContentView.swift
@@ -7,15 +7,79 @@
 
 import SwiftUI
 
+let backgroundColor = Color("forBg")
+
 struct ContentView: View {
+    //buat variabel untuk isi text field nya
+    @State var textFieldValue: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image("test")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                .padding(.top, 62)
+            
+            Image("test2")
+                .resizable()
+//              .frame(width: 200.87 ,height: 147)
+                .scaledToFit()
+                .scaleEffect(0.6)
+                .padding(.top, -124)
+            
+            Text("Insert your own storymoji!")
+                .foregroundColor(Color("darkOrange"))
+                .font(.system(.title2, design: .rounded))
+                .bold()
+                .padding(.top, -44)
+            
+            //langsung dipanggil aja variabel nya
+            TextField("", text: $textFieldValue)
+                .padding()
+                .frame(maxWidth: 290, maxHeight: 46)
+                .background(.white)
+                .cornerRadius(8)
+                .shadow(color: Color.black.opacity(0.22), radius: 9.0, x: 0.0, y: 1.0)
+//            Spacer()
+            
+            VStack{
+                Button {
+                    print(textFieldValue)
+                } label: {
+                    Image(systemName: "shareplay")
+                        .imageScale(.large)
+                    
+                    Text("Be a Host")
+                        .font(.system(.title2, design: .rounded))
+                        .bold()
+                }
+                .frame(width: 196, height: 55)
+                .background(Color("darkOrange"))
+                .foregroundColor(backgroundColor)
+                .cornerRadius(17)
+                
+                Button {
+                    print("Peer")
+                } label: {
+                    Image(systemName: "person.fill")
+                        .imageScale(.medium)
+                    
+                    Text("Join a Game")
+                        .font(.system(.title2, design: .rounded))
+                        .bold()
+                }
+                .frame(width: 220, height: 55)
+                .background(Color("darkOrange"))
+                .foregroundColor(backgroundColor)
+                .cornerRadius(17)
+                .padding(.top, 15)
+            }
+            .padding(.top, 45)
+            Spacer()
         }
-        .padding()
+        .frame(maxWidth: .infinity)
+        .background(backgroundColor)
+        
     }
 }
 
